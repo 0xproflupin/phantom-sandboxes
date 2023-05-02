c93285ee8eb0018354db3d44e0ca8e18e862cbce
0xproflupin <mac.anvit@gmail.com>
2023-05-02 14:17:42 +0530

    fix domain

--- src/sandboxes/experimental-sandbox/utils/createSignInData.ts
+++ src/sandboxes/experimental-sandbox/utils/createSignInData.ts
@@ -6,7 +6,7 @@ const createSignInData = async (address: string): Promise<SignInData> => {
   // Convert the Date object to a string
   const currentDateTime = now.toString();
   const signInData: SignInData = {
-    domain: "vercel.app",
+    domain: "phantom-sandboxes.vercel.app",
     address,
     statement: "Sign-in to connect!",
     uri: "https://phantom-sandboxes.vercel.app/",
