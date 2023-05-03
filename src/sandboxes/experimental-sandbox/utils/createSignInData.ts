afac64e43e482ed8eabd974799d2603081c8e8a6
0xproflupin <mac.anvit@gmail.com>
2023-05-04 00:40:10 +0530

    generalise signin message generation

--- src/sandboxes/experimental-sandbox/utils/createSignInData.ts
+++ src/sandboxes/experimental-sandbox/utils/createSignInData.ts
@@ -2,19 +2,22 @@ import { SignInData } from '../types';
 
 const createSignInData = async (address: string): Promise<SignInData> => {
   const now: Date = new Date();
+  const uri = window.location.href
+  const currentUrl = new URL(uri);
+  const domain = currentUrl.hostname.replace(/^www\./, '');
 
   // Convert the Date object to a string
   const currentDateTime = now.toISOString();
   const signInData: SignInData = {
-    domain: "phantom-sandboxes.vercel.app",
+    domain,
     address,
     statement: "Sign-in to connect!",
-    uri: "https://phantom-sandboxes.vercel.app/",
+    uri,
     version: "1",
     nonce: "oBbLoEldZs",
     chain: "mainnet",
     issuedAt: currentDateTime,
-    // resources: ["https://example.com", "https://phantom.app/"]
+    resources: ["https://example.com", "https://phantom.app/"]
   };
 
   return signInData;
